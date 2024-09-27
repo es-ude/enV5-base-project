38c3d6a9627b99520411cb2979f59431d8e7231a
David P. Federl <david-peter.federl@federl.digital>
2024-09-27 18:19:36 +0200

    build(all): add pre-commit

---
+++ .commitlintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends : [ '@commitlint/config-conventional' ],
+  rules : {
+    'type-empty' : [ 2, "never" ],
+    'type-case' : [ 2, "always", "lower-case" ],
+    'type-enum' :
+                [
+                  2, "always",
+                  [
+                    'build', 'ci', 'feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore',
+                    'wip', 'perf'
+                  ]
+                ],
+    'scope-empty' : [ 2, "never" ],
+    'scope-case' : [ 2, "always", "kebab-case" ],
+    'scope-enum' :
+                 [
+                   2, "always",
+                   [ 'hardware-test', 'unit-test', 'all', 'workflow', 'readme', 'cmake', 'demo' ]
+                 ],
+    'subject-empty' : [ 2, "never" ],
+  }
+}
